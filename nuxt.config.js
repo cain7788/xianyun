c893d0eac862e13cbcb747c0811ba34ef05d6a3f
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-10-12 01:06:51 +0800

    完成首页编辑，header和footer组件调用

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,14 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '咸鱼旅游网',
+    title:'咸鱼旅游网',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon1.png' },
       { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'} // 新增全局字体样式
     ]
   },
---
+++ pages/lianxi.vue
@@ -0,0 +1,67 @@
+<template>
+  <div style="padding:20px;">
+      <div class="tap">
+          <span 
+          v-for="(item,index) in ['新闻','体育','娱乐',]" 
+          :key="index"
+          @click="handleClick(index)"
+          :class="{active:current === index}"
+          >{{item}}</span>
+      </div>
+      <div v-if="current === 0">
+          <p>2019年诺贝尔和平奖揭晓 埃塞俄比亚总理获奖</p>
+          <p>2019年诺贝尔和平奖揭晓 埃塞俄比亚总理获奖</p>
+          <p>2019年诺贝尔和平奖揭晓 埃塞俄比亚总理获奖</p>
+          <p>2019年诺贝尔和平奖揭晓 埃塞俄比亚总理获奖</p>
+      </div>
+      <div v-if="current === 1">
+          <p>世预赛-杨旭4球武磊传射艾克森破门 国足7-0关岛</p>
+          <p>世预赛-杨旭4球武磊传射艾克森破门 国足7-0关岛</p>
+          <p>世预赛-杨旭4球武磊传射艾克森破门 国足7-0关岛</p>
+          <p>世预赛-杨旭4球武磊传射艾克森破门 国足7-0关岛</p>
+          
+      </div>
+      <div v-if="current === 2">
+          <p>说声恭喜？曝黄渤喜得爱子 育有两个女儿生活幸福</p>
+          <p>说声恭喜？曝黄渤喜得爱子 育有两个女儿生活幸福</p>
+          <p>说声恭喜？曝黄渤喜得爱子 育有两个女儿生活幸福</p>
+          <p>说声恭喜？曝黄渤喜得爱子 育有两个女儿生活幸福</p>
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            options:[],
+            current:0,
+        }
+    },
+
+    methods:{
+        handleClick(index){
+            this.current = index
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .tap {
+        padding: 20px;
+
+       span{
+           padding: 5px;
+           border: 1px #ddd solid;
+           margin-right: 15px;
+           cursor:pointer;
+       }
+
+       .active {
+           border: 1px solid red;
+           color: red;
+       }
+    }
+</style>
