3d7babb1272a9c4d8196c0fda1cecf16e06560f6
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-10-14 16:50:09 +0800

    完成添加错误拦截器
    context是nuxt下的所有方法，$axios中有一个方法是onError是拦截错误的信息。
    通过res.response将返回的错误信息打印出来

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,42 @@
-// import {Message} from "element-ui";
+import {Message} from "element-ui";
+
+// context是nuxt下的所有方法，其中包含了$axios的方法
+export default (context) => {
+
+    console.dir(context.$axios);
+    
+    // 调用$axios的方法进行拦截
+    context.$axios.onError(res=>{
+        // res是错误的对象, Error的对象可以通过response获取详细信息
+        // console.log(res);
+        // res.response 是原生js中的方法
+        const {message, statusCode} = res.response.data;
+
+                if(statusCode === 400){
+                    Message.warning(message)
+                }
+            
+    })
+}
+
+
+// import { Message } from 'element-ui';
+
+// // nuxt插件的固定写法
+// // context包含nuxt下所有的方法，固定有的参数
+// export default (context) => {
+//     // 拦截错误的响应信息， 根以前项目的中main.js中拦截器不一样.
+//     // main.js中拦截器: 拦截所有的请求响应
+//     // 当前的拦截只拦截错误,如果请求错误就会执行onError中的函数
+//     context.$axios.onError(res => {
+//         // res是错误的对象, Error的对象可以通过response获取详细信息
+//         const {message, statusCode} = res.response.data;
+
+//         if(statusCode === 400){
+//             Message.error(message)
+//         }
+//     })
+// }
 
 // export default function({$axios, redirect}){
 //     $axios.onError(err => {
@@ -15,12 +53,3 @@
 //         }
 //     })
 // }
-
-// content是nuxt下的所有方法，其中包含了$axios的方法
-export default (content) => {
-
-    console.log(content.$axios);
-    
-    // 调用$axios的方法进行拦截
-    // content.$axios
-}
--- store/user.js
+++ store/user.js
@@ -59,3 +59,5 @@ export const actions = {
         return res
     }
 }
+
+
